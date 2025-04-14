88e19bc4d677650d7ee2ffc2f8e7fd459ea81351
BeniAnalyticss <bhardoy@mail.austral.edu.ar>
2025-04-14 12:13:24 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,11 @@
 def leap_year():
-    print("TO DO")
+    year = int(input("Ingrese el año: "))
+    print(year)
+    if year%4 == 0:
+        if year%100 == 0  and year%400 == 0:
+         print(f"El año {year} es bisiesto")
+       
+        elif year%100 != 0:
+            print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
